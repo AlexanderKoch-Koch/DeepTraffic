a600ec5dfdd1da1866dbd5a0eae73e417041b4d6
AlexanderKoch-Koch <alexander@4koch.de>
2018-01-24 22:06:46 +0100

    most parameters working

--- Agent.py
+++ Agent.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import re
 
 
 class Agent:
@@ -13,16 +14,16 @@ class Agent:
     def run(self, queue):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
+        options.add_argument('--start-maximized')
+        options.add_argument('--start-fullscreen')
         driver = webdriver.Chrome(options=options)
         driver.get("https://selfdrivingcars.mit.edu/deeptraffic/")
 
         # change code
-        # driver.execute_script('''var editor = document.getElementsByClassName("view-lines")[0];editor.innerText = "''' + self.generate_code(self.params) + '"')
-        #editor = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div[1]')
         code = self.generate_code(self.params)
         print(code)
         driver.execute_script('editor.setValue("' + code + '")')
-        driver.save_screenshot('screenshot1.png')
+
         # click apply code button
         driver.find_element_by_xpath('//*[@id="main"]/div/div[3]/div[3]/button[1]').click()
 
@@ -30,81 +31,91 @@ class Agent:
         train_button = driver.find_element_by_id("trainButton")
         train_button.click()
         try:
-            element = WebDriverWait(driver, 1000).until(
+            element = WebDriverWait(driver, 10000).until(
                 EC.element_to_be_clickable((By.ID, "trainButton"))
             )
+        except TimeoutException:
+            queue.put(0)
+            return
         finally:
             print("training finished")
             # click OK button
             driver.find_element_by_class_name("confirm").click()
+            #driver.execute_script("window.scrollBy(100,250)")
+            #driver.save_screenshot('training.png')
 
-        time.sleep(0.1)
+
+        time.sleep(0.2)
         eval_button = driver.find_element_by_xpath('//*[@id="evalButton"]')
         eval_button.click()
         try:
-            element = WebDriverWait(driver, 1000).until(
+            element = WebDriverWait(driver, 10000).until(
                 EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[7]/div/button"))
             )
+        except TimeoutException:
+            queue.put(0)
+            return
         finally:
-            print("evaluation finished")
             time.sleep(0.1)
-            driver.save_screenshot('screenie.png')
             text_result = driver.find_element_by_xpath("/html/body/div[3]/p/b")
             result_string = text_result.get_attribute('innerHTML')
-            print(result_string)
-            speed = float(result_string[0:5])
+            speed_string = re.findall("\d+\.\d+", result_string)[0]
+            speed = float(speed_string)
 
         queue.put(speed)
 
     @staticmethod
     def generate_code(params):
-        print(params)
-        code =  'lanesSide = ' + str(params[0]) + ';\\n' + \
-                'patchesAhead = ' + str(params[1]) + ';\\n' + \
-                'patchesBehind = ' + str(params[2]) + ';\\n' + \
-                'trainIterations = ' + str(params[3]) + ';\\n' + \
+        #print(int(params[0]))
+        code =  'lanesSide = ' + str(int(params[0])) + ';\\n' + \
+                'patchesAhead = ' + str(int(params[1])) + ';\\n' + \
+                'patchesBehind = ' + str(int(params[2])) + ';\\n' + \
+                'trainIterations = ' + str(int(params[3])) + ';\\n' + \
                 'otherAgents = 0;\\n' + \
                 'var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);\\n' + \
                 'var num_actions = 5;\\n' + \
-                'var temporal_window = 3;\\n' + \
+                'var temporal_window = ' + str(int(params[12])) + ';\\n' + \
                 'var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;\\n' + \
                 'var layer_defs = [];\\n' + \
-                '    layer_defs.push({\\n' + \
-                '    type: "input",\\n' + \
+                'layer_defs.push({\\n' + \
+                '    type: \'input\',\\n' + \
                 '    out_sx: 1,\\n' + \
                 '    out_sy: 1,\\n' + \
                 '    out_depth: network_size\\n' + \
                 ' });\\n' + \
                 'layer_defs.push({\\n' + \
-                '    type: "fc",\\n' + \
-                '    num_neurons = ' + str(params[4]) + ',\\n' + \
-                '    activation: "relu"\\n' + \
-                '});\\n' + \
-                'layer_defs.push({\\n' + \
-                '    type: "fc",\\n' + \
-                '    num_neurons = ' + str(params[5]) + ',\\n' + \
-                '    activation: "relu"\\n' + \
-                '});\\n' + \
-                'layer_defs.push({\\n' + \
-                '    type: "fc",\\n' + \
-                '    num_neurons = ' + str(params[6]) + ',\\n' + \
-                '    activation: "relu"\\n' + \
-                '});\\n' + \
-                'layer_defs.push({\\n' + \
-                '    type: "regression",\\n' + \
+                '    type: \'fc\',\\n' + \
+                '    num_neurons: ' + str(int(params[4])) + ',\\n' + \
+                '    activation: \'relu\'\\n' + \
+                '});\\n'
+        if(params[5] > 0):
+            code += 'layer_defs.push({\\n' + \
+                    '    type: \'fc\',\\n' + \
+                    '    num_neurons: ' + str(int(params[5])) + ',\\n' + \
+                    '    activation: \'relu\'\\n' + \
+                    '});\\n'
+            if params[6] > 0:
+                code += 'layer_defs.push({\\n' + \
+                        '    type: \'fc\',\\n' + \
+                        '    num_neurons: ' + str(int(params[6])) + ',\\n' + \
+                        '    activation: \'relu\'\\n' + \
+                        '});\\n'
+
+        code += 'layer_defs.push({\\n' + \
+                '    type: \'regression\',\\n' + \
                 '    num_neurons: num_actions\\n' + \
                 '});\\n' + \
                 'var tdtrainer_options = {\\n' + \
-                '    learning_rate: 0.001,\\n' + \
-                '    momentum: 0.0,\\n' + \
-                '    batch_size: 64,\\n' + \
-                '    l2_decay: 0.01\\n' + \
+                '    learning_rate: ' + str(params[7]) + ',\\n' + \
+                '    momentum: ' + str(params[8]) + ',\\n' + \
+                '    batch_size: ' + str(int(params[9])) + ',\\n' + \
+                '    l2_decay: ' + str(params[10]) + '\\n' + \
                 '};\\n' + \
                 'var opt = {};\\n' + \
                 '    opt.temporal_window = temporal_window;\\n' + \
                 '    opt.experience_size = 3000;\\n' + \
                 '    opt.start_learn_threshold = 500;\\n' + \
-                '    opt.gamma = 0.88;\\n' + \
+                '    opt.gamma = ' + str(params[11]) + ';\\n' + \
                 '    opt.learning_steps_total = 10000;\\n' + \
                 '    opt.learning_steps_burnin = 1000;\\n' + \
                 '    opt.epsilon_min = 0.0;\\n' + \
@@ -118,8 +129,6 @@ class Agent:
                 '        draw_net();\\n' + \
                 '        draw_stats();\\n' + \
                 '        return action;\\n' + \
-                '        return action;\\n' + \
-                '        }\\n' + \
-                '//]]>'
+                '    }\\n'
 
         return code
--- DeepTraffic.py
+++ DeepTraffic.py
@@ -1,39 +1,72 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+from Agent import Agent
+import random
+import numpy as np
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Process, Queue
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-driver.get("https://selfdrivingcars.mit.edu/deeptraffic/")
+pool = ThreadPool(processes=1)
 
-train_button = driver.find_element_by_id("trainButton")
-train_button.click()
-train_success = False
-try:
-    element = WebDriverWait(driver, 1000).until(
-        EC.element_to_be_clickable((By.ID, "trainButton"))
-    )
-finally:
-    print("finished")
-    #click OK button
-    driver.find_element_by_class_name("confirm").click()
-train_success = True
+# evolution parameters
+generations = 5
+num_agents = 16
 
-if(train_success):
-    time.sleep(0.1)
-    eval_button = driver.find_element_by_xpath('//*[@id="evalButton"]')
-    eval_button.click()
-    eval_success = False
-    try:
-        element = WebDriverWait(driver, 1000).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[7]/div/button"))
-        )
-    finally:
-        print("finished")
-        time.sleep(0.1)
-        driver.save_screenshot('screenie.png')
-        text_result = driver.find_element_by_xpath("/html/body/div[3]/p/b")
-        print(text_result.get_attribute('innerHTML'))
+# start parameters for first gen agents
+params = [1,        # lanesSide
+          7,        # patchesAhead
+          0,        # patchesBehind
+          5000,     # trainIterations
+          10,       # l1_num_neurons
+          0,        # l2_num_neurons
+          0,        # l3_num_neurons
+          0.001,    # learning_rate
+          0.0,      # momentum
+          64,       # batch_size
+          0.01,     # l2_decay
+          0.88,     # gamma
+          1]        # temporal window
+
+for i in range(generations):
+    # for each generation
+    print("generation: " + str(i))
+    parameters = []
+    scores = []
+    queues = []
+    processes = []
+    agents = np.zeros((num_agents, len(params) + 1), dtype=np.float32)  # gamma, epsilon, learning_rate, neurons, score
+    for a in range(num_agents):
+        # for each agent a in generation i
+        agents[a][0] = int(max(1, random.gauss(mu=params[0], sigma=0.1))) # lanesSide
+        agents[a][1] = int(max(1, random.gauss(mu=params[1], sigma=0.5)))  # patchesAhead
+        agents[a][2] = int(max(0, random.gauss(mu=params[2], sigma=0.4)))  # patchesBehind
+        agents[a][3] = int(max(1, random.gauss(mu=params[3], sigma=100)))    # trainIterations
+        agents[a][4] = int(max(1, random.gauss(mu=params[4], sigma=1.5)))  # l1_num_neurons
+        agents[a][5] = int(max(0, random.gauss(mu=params[5], sigma=1)))  # l2_num_neurons
+        agents[a][6] = int(max(0, random.gauss(mu=params[6], sigma=0.5)))  # l3_num_neurons
+        agents[a][7] = random.gauss(mu=params[7], sigma=0.0001)  # learning_rate
+        agents[a][8] = random.gauss(mu=params[8], sigma=0)  # momentum
+        agents[a][9] = random.gauss(mu=params[9], sigma=2)  # batch_size
+        agents[a][10] = random.gauss(mu=params[10], sigma=0.001)  # l2_decay
+        agents[a][11] = random.gauss(mu=params[11], sigma=0.01)  # gamma
+        agents[a][12] = int(max(1, random.gauss(mu=params[12], sigma=0.5)))  # temporal window
+
+        agent = Agent(agents[a][0:len(agents[a] - 1)])
+        queues.append(Queue())
+        processes.append(Process(target=agent.run, args=(queues[a],)))
+        processes[a].start()
+
+    #print("launched all threads")
+    score_index = len(params)
+    for a in range(num_agents):
+        agents[a][score_index] = queues[a].get()
+
+    # sort agents by score
+    agents = agents[np.argsort(agents[:, score_index])]
+    print(agents[:, score_index])
+    # take mean of the 2 best agents parameters
+    for parameter in range(0, len(params) - 1):
+        params[parameter] = np.mean(agents[-2, parameter])
+
+    # print values of ten best agents
+    #print(agents[-10:, :])
+    print(params)
+
